1441a6a7ba935bb43028d4b1447dcd4b4f4aba43
Jacob Archambault <jacobarchambault@gmail.com>
2021-03-23 19:43:50 -0400

    Move all declarations and initializations to header from start()
    In order to facilitate process of refactoring into separate classes.

--- src/main/java/com/jacobarchambault/nameformatter/NameFormatter.java
+++ src/main/java/com/jacobarchambault/nameformatter/NameFormatter.java
@@ -12,76 +12,86 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 
 public class NameFormatter extends Application {
+	TextField fnTextField = new TextField();
+	HBox fnhbox = new HBox(
+			10,
+			new Label(
+					"First name:"),
+			fnTextField);
+	Label mnprompt = new Label(
+			"Middle name:");
+	TextField mnTextField = new TextField();
+	HBox mnhbox = new HBox(
+			10,
+			mnprompt,
+			mnTextField);
+	Label lnprompt = new Label(
+			"Last name:");
+	TextField lnTextField = new TextField();
+	HBox lnhbox = new HBox(
+			10,
+			lnprompt,
+			lnTextField);
+	Label tprompt = new Label(
+			"Title:");
+	TextField tTextField = new TextField();
+	HBox thbox = new HBox(
+			10,
+			tprompt,
+			tTextField);
+	Button button1 = new Button(
+			"Format 1");
+	Button button2 = new Button(
+			"Format 2");
+	Button button3 = new Button(
+			"Format 3");
+	Button button4 = new Button(
+			"Format 4");
+	Button button5 = new Button(
+			"Format 5");
+	Button button6 = new Button(
+			"Format 6");
+	HBox bhbox = new HBox(
+			10,
+			button1,
+			button2,
+			button3,
+			button4,
+			button5,
+			button6);
+	Label outputLabel = new Label();
+	HBox outputHBox = new HBox(
+			outputLabel);
+	VBox masterVBox = new VBox(
+			10,
+			fnhbox,
+			mnhbox,
+			lnhbox,
+			thbox,
+			bhbox,
+			outputHBox);
+	Scene scene = new Scene(
+			masterVBox);
 
 	@Override
 	public void start(
 			Stage primaryStage) throws Exception {
 		// Create the controls for the first name.
-		TextField fnTextField = new TextField();
-		HBox fnhbox = new HBox(
-				10,
-				new Label(
-						"First name:"),
-				fnTextField);
 		fnhbox.setAlignment(
 				Pos.CENTER);
 		// Create the controls for the middle name.
-		Label mnprompt = new Label(
-				"Middle name:");
-		TextField mnTextField = new TextField();
-		HBox mnhbox = new HBox(
-				10,
-				mnprompt,
-				mnTextField);
 		mnhbox.setAlignment(
 				Pos.CENTER);
 		// Create the controls for the last name.
-		Label lnprompt = new Label(
-				"Last name:");
-		TextField lnTextField = new TextField();
-		HBox lnhbox = new HBox(
-				10,
-				lnprompt,
-				lnTextField);
 		lnhbox.setAlignment(
 				Pos.CENTER);
 		// Create the controls for the title.
-		Label tprompt = new Label(
-				"Title:");
-		TextField tTextField = new TextField();
-		HBox thbox = new HBox(
-				10,
-				tprompt,
-				tTextField);
 		thbox.setAlignment(
 				Pos.CENTER);
 		// Create the Button Controls.
-		Button button1 = new Button(
-				"Format 1");
-		Button button2 = new Button(
-				"Format 2");
-		Button button3 = new Button(
-				"Format 3");
-		Button button4 = new Button(
-				"Format 4");
-		Button button5 = new Button(
-				"Format 5");
-		Button button6 = new Button(
-				"Format 6");
-		HBox bhbox = new HBox(
-				10,
-				button1,
-				button2,
-				button3,
-				button4,
-				button5,
-				button6);
 		bhbox.setAlignment(
 				Pos.CENTER);
 		// Create the output label.
-		Label outputLabel = new Label();
-		HBox outputHBox = new HBox(
-				outputLabel);
 		// Register event handlers.
 		button1.setOnAction(
 				e -> {
@@ -116,19 +126,9 @@ public class NameFormatter extends Application {
 							lnTextField.getText() + ", " + mnTextField.getText());
 				});
 		// Create a master VBox
-		VBox masterVBox = new VBox(
-				10,
-				fnhbox,
-				mnhbox,
-				lnhbox,
-				thbox,
-				bhbox,
-				outputHBox);
 		masterVBox.setPadding(
 				new Insets(
 						10));// Add the master VBox to a scene.
-		Scene scene = new Scene(
-				masterVBox);
 		// Set the scene to the stage and display it.
 		primaryStage.setScene(
 				scene);
